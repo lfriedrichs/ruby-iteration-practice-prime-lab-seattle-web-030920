13cf61e7f494fe60f790117b4e919e9a3b008f08
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-29 18:41:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number) 
+  i = 2
+  if number < 2 
+    output = false 
+  else
+    output = true
+    while i < number
+      if number % i == 0
+        output = false 
+      end
+      i += 1 
+    end
+  end
+  output
+end 
